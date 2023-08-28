fe013eacb97a12f83b9e0ed851806c215ee1de25
Kuman13 <kyym13@gmail.com>
2023-08-28 21:56:40 +0200

    added 2 new files

---
+++ hw2.js
@@ -0,0 +1,53 @@
+//let x = 5;
+//let y = 5;
+
+//let resN = `${x * y}`
+
+//console.log(resN)
+
+
+let str = "Hello World!";
+
+let lowerCaseStr = str.toLowerCase(); // "hello world!"
+console.log(lowerCaseStr)
+
+
+let upperCaseStr = str.toUpperCase(); // "HELLO WORLD!"
+console.log(upperCaseStr)
+
+let let1 = "This is a boom text.";
+let replacedText = let1.replace("boom", "BANG");
+console.log(replacedText); 
+
+let x = 5;
+let y = 10;
+
+if (x != y) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+let number = 5; 
+number += 3;  
+
+console.log(number);  
+
+let isEqual = (x == y)
+
+console.log(isEqual)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- operations.js
+++ operations.js
@@ -1,6 +1,86 @@
-let num1 = 3
-let num2 = 5
+// let num1 = 3
+// let num2 = 5
+// let resN = `${num1 + num2}`
 
-let resN = `${num1} + ${num2}`
+// let operPercent = 5%2
 
-console.log(resN)
+// let operModul = 2 ** 10
+// console.log(operModul)
+
+// let x = 5
+// ++x
+// console.log(x)
+
+
+//comparison operations
+let x = 5
+let x2 = 'i like to play basketl NOT'
+
+//console.log(typeof x)
+// x2 = Number(x2)
+//console.log(typeof x2)
+
+
+//let resComparison = x !== x2
+//console.log(resComparison)
+
+
+//let myComp = 33 <= 100
+
+
+// operators logical
+
+let t1 = true
+let t2 = true
+let logicOperAnd = (x > 1 && x < 6)
+
+//console.log(logicOperAnd)
+
+let logicOperOr = (x == 5 || x >= 100)
+
+console.log(logicOperOr)
+
+//(x&&y || x >=100 && x==100)
+
+let logicOperNot = !(x == 5)
+console.log(logicOperNot)
+
+//String operations
+//let lX2 = x2.length
+let partX2 = x2.slice(15, 23)
+//console.log(partX2)
+
+
+let partX3 = x2.substring(15, 22)
+console.log(partX3)
+
+console.log(x2)
+let n = 'not'
+x2 = x2.replace( n, '@YES@')
+n = n.toUpperCase()
+x2 = x2.replace( n, '@YES@')
+console.log(x2)
+
+n = n.toLowerCase()
+let txtConcat = partX3.concat(' ', n)
+console.log(txtConcat)
+
+let txtWithTrash = '          original Text          '
+
+console.log(txtWithTrash.trim())
+
+
+
+
+//x typeof x ==> Number
+
+
+console.log(Number.isInteger(x));
+
+
+
+
+
+
+
+
